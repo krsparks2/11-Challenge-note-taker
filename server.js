42ea85b9a5f5a331a104a73d05ea4952c6e00ee3
Keli <krsparks2@yahoo.com>
2022-09-04 16:33:12 -0700

    initial push with starter code

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const htmlRoutes = require('./routes/html')
+const apiRoutes = require('./routes/api')
+
+//Create local host port
+const PORT = process.env.PORT || 3001;
+
+//Create express app
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
